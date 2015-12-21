6ee8e553074db45a8490e421f2bf1d5ab0ccbb7c
ImNotMax <maxime.sespin@gmail.com>
2015-12-20 23:55:49 -0500

    encore des tests

--- install.bash
+++ install.bash
@@ -3,7 +3,8 @@
 InstallerPath=$(PWD)
 CurrentUser=$(USERS)
 findme="forever-agent"
-
+echo
+echo
 ## check if NPM Forever agent is installed
 file=$(npm list | grep $findme)
 
@@ -16,18 +17,18 @@ else
 	echo "$findme found: $file"
 fi
 
-
-
+echo
+echo
 echo -------------------------------
 ## create autostart plist for next boot
 echo 'Installing Homebridge-Daemon...'
 echo Install from: $InstallerPath
-echo Install for: $CurrentUser or ${CurrentUser}
 echo -------------------------------
 echo
 
-## copy the bash file used in the PList file to launch Homebridge through Forever
-cp ${InstallerPath}/forever-homebridge.bash /usr/local/bin/forever-homebridge.bash
+## Copy the bash file used in the PList file to launch Homebridge through Forever
+cp ${InstallerPath}/homebridge-daemon.bash /usr/local/bin/homebridge-daemon.bash
+
 
 ## Modify the VAR names in the PList file and save it in the Library directory
 sed -e "s/__CURRENTUSER__/${CurrentUser}/" "${InstallerPath}/org.homebridge.daemon.plist" > /Library/LaunchDaemons/org.homebridge.daemon.plist
@@ -36,17 +37,19 @@ sed -e "s/__CURRENTUSER__/${CurrentUser}/" "${InstallerPath}/org.homebridge.daem
 ##cp ${InstallerPath}/org.homebridge.daemon.plist /Library/LaunchDaemons/org.homebridge.daemon.plist
 
 ## change ownership and permissions of the plist file to make it launchctl compatible
-chown root /Library/LaunchDaemons/org.homebridge.daemon.plist
+chmod 755 /usr/local/bin/homebridge-daemon.bash
+chown root:wheel /Library/LaunchDaemons/org.homebridge.daemon.plist
 chmod 644 /Library/LaunchDaemons/org.homebridge.daemon.plist
 
 ## start Homebridge-Daemon for this session
 echo 'Starting Homebridge-Daemon...'
-
+echo
 ## launch the plist so that we can use it without a reboot
 launchctl load /Library/LaunchDaemons/org.homebridge.daemon.plist
+## sh /usr/local/bin/forever-homebridge.bash
 
 ## wait a couple seconds to allow Homebridge to load
-## sleep 5
+sleep 5
 
 ## display the running status of Homebridge
 echo   - below the running status of Homebridge-daemon.
@@ -54,5 +57,8 @@ echo   - if nothing appears, the daemon is not launched
 echo
 launchctl list | grep org.homebridge.daemon
 echo
+echo
+echo display if Homebridge has been started with Forever:
+forever list | grep "Homebridge"
 echo
 ## end
--- uninstall.bash
+++ uninstall.bash
@@ -10,5 +10,8 @@ forever stop /usr/local/bin/homebridge
 ## remove autostart plist from the /Library/LaunchDameons folder
 rm /Library/LaunchDaemons/org.homebridge.daemon.plist
 
+## remove the bash file used to launch the daemon
+rm /usr/local/bin/homebridge-daemon.bash
+
 ## display the running status of Homnebridge Daemon
 launchctl list | grep org.homebridge.daemon
